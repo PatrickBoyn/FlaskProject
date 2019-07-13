13d3aaa23d02ce273b45e2e5c6159884f5192bdb
Patrick Boynton <dakilangmanok@gmail.com>
2019-07-13 00:20:55 -0400

    Created the second method.

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Remember the s at the end of methods.
+# POST a new store.
 @app.route('/store', methods=['POST'])
 def create_store():
     pass
+
+@app.route('/store')
+def get_store():
+    pass
+
 
 app.run(port=5500, debug=True)
