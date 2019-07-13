50b6f27d7209df76c26f04e99035ae3581ec0e7c
Patrick Boynton <dakilangmanok@gmail.com>
2019-07-13 00:19:00 -0400

    Completely redoing because I got lost in the code.

--- app.py
+++ app.py
@@ -2,53 +2,9 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-stores = [
-    {
-        'name': 'Test store 1',
-        'items': [
-            {
-                'name': 'apples',
-                'price': 3.45,
-            }
-        ]
-    }
-]
-
+# Remember the s at the end of methods.
 @app.route('/store', methods=['POST'])
 def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    
-    stores.append(new_store)
-    
-    return jsonify(new_store)
-
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-
-    return jsonify({'message': 'Store not found.'})
-            
-
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_store_item(name):
     pass
-
-@app.route('/store/<string:name>/item')
-def get_store_item(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({store['items']})
-
-    return jsonify({'message': 'Item not found.'})
 
 app.run(port=5500, debug=True)
